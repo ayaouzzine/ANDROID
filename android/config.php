07be858a6b2637d814a53b508da8f1e3e4290aa2
ayaouzzine <58732316+ayaouzzine@users.noreply.github.com>
2021-05-31 23:10:01 +0100

    Changed config file for remote db connexion

--- android/config.php
+++ android/config.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "android");
+$conn = new mysqli("sql11.freemysqlhosting.net", "sql11414667", "bZP4vUa2E1", "sql11414667");
 
 // Check connection
 if ($conn->connect_error) {
